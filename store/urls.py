c9348c0370d6c2428835a1ea0d5e5e9c71890d62
Babenko Denis <babenko.denis3009@gmail.com>
2019-09-13 15:05:04 +0300

    add test html and css files

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
+    path('', index),
+    path('products/', products),
+    path('product/', product),
+    path('auth/', auth),
+    path('profile/', profile),
+    path('basket/', basket),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'store/main.html', context={})
+
+
+def products(request):
+    return render(request, 'store/products.html', context={})
+
+
+def product(request):
+    return render(request, 'store/product.html', context={})
+
+
+def basket(request):
+    return render(request, 'store/basket.html', context={})
+
+
+def profile(request):
+    return render(request, 'store/profile.html', context={})
+
+
+def auth(request):
+    return render(request, 'store/auth.html', context={})
